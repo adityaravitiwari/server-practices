15c21734f2b910255d23798b5efbb4b22fb114de
ravi ranjan <adityaravitiwari022160@gmail.com>
2026-02-24 23:27:27 +0530

    final comit

--- utils/File.js
+++ utils/File.js
@@ -1,15 +1,30 @@
 const fs = require("fs");
 
-const serveFile = (res, filePath, contentType, statusCode = 200) => {
-  fs.readFile(filePath, (err, data) => {
+const serveFile = (res, fPath, contentType,status=200) =>
+   {
+  fs.readFile(fPath, (err, data) =>
+     {
     if (err) {
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Internal Server Error");
-    } else {
-      res.writeHead(statusCode, { "Content-Type": contentType });
-      res.end(data);
-    }
+      res.writeHead(500);
+      return res.end(" Error");} 
+    else {res.writeHead(status);
+       return res.end(data);}
   });
 };
 
 module.exports = serveFile;
+
+/* Here Not Ai not used here .
+
+i just created A file js where i used express  tool (Fs) to read   and write in The file . 
+here i take res , and filepath to add and get from respected file , 200 status code is for succes and 500 for eror ;
+
+
+
+*/
+
+
+
+
+
+
